fc70cf884296beedab961fa36ab34ead3df44b46
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2019-04-29 19:44:37 -0300

    Criação do Menu
    1. Criada a estrutura base para o menu;
    2. Faltando implementar as funções para os botões e aplicar a logo e o parallax.

--- RPG/RPG/MainPage.xaml.cs
+++ RPG/RPG/MainPage.xaml.cs
@@ -17,28 +17,52 @@ namespace RPG
 
         public MainPage()
         {
-            window = new Window(this, 840, 620);
+            Window window = new Window(this, 840, 620);
+            this.InitializeComponent();
 
+            UButton start = new UButton("Iniciar", new Vector2(50, 60), new Vector2(150,50), Start);
+            UButton settings = new UButton("Configurações", new Vector2(50, 70), new Vector2(150, 50), Settings);
+            UButton exit = new UButton("Sair", new Vector2(50, 80), new Vector2(150, 50), Exit);
 
+            window.AddUI(start);
+            window.AddUI(settings);
+            window.AddUI(exit);
 
-            UButton btn = new UButton("Jander",new Vector2(0,0),new Vector2(100,50), algumaCoisa);
-          
-            InitializeComponent();
-            window.AddUI(btn);
-           
-         
             AmbienteJogo.Init(window);
-            
-            new Caixa(new Vector2(-200, 0));
-          
-            new Player(new Vector2(0, 0));
-           
-            AmbienteJogo.Execute(120);
+
+            /*  window = new Window(this, 840, 620);
+
+
+
+              UButton btn = new UButton("Jander",new Vector2(0,0),new Vector2(100,50), algumaCoisa);
+
+              InitializeComponent();
+              window.AddUI(btn);
+
+
+              AmbienteJogo.Init(window);
+
+              new Caixa(new Vector2(-200, 0));
+
+              new Player(new Vector2(0, 0));
+
+              AmbienteJogo.Execute(120);*/
         }
 
         private void algumaCoisa(object sender)
         {
             Engine.Debug("Alguma coisa ocorreu");
+        }
+        private void Start(object Sender) {
+            // Iniciar jogo;
+        }
+
+        private void Settings(object Sender) {
+            // Abrir aba de configurações;
+        }
+
+        private void Exit(object Sender) {
+            Environment.Exit(0);
         }
     }
 }
---
+++ RPG/RPG/Src/Scripts/Interface de Usuário/Menu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using _3ReaisEngine.RPG.Core;
+using _3ReaisEngine.Core;
+using _3ReaisEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using _3ReaisEngine;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RPG.Src.Scripts.Interface_de_Usuário {
+
+    public sealed partial class Menu : Page {
+
+        public Menu() {
+
+            Window window = new Window(this, 330, 290);
+            this.InitializeComponent();
+
+            UButton start = new UButton("Iniciar", new Vector2(100, 100), Start);
+            UButton settings = new UButton("Configurações", new Vector2(50, 40), Settings);
+            UButton exit = new UButton("Sair", new Vector2(50, 40), Exit);
+
+            window.AddUI(start);
+            window.AddUI(settings);
+            window.AddUI(exit);
+
+            AmbienteJogo.Init(window);
+
+        }
+
+        private void Start(object Sender) {
+            // Iniciar jogo;
+        }
+
+        private void Settings(object Sender) {
+            // Abrir aba de configurações;
+        }
+
+        private void Exit(object Sender) {
+            Environment.Exit(0);
+        }
+    }
+}
