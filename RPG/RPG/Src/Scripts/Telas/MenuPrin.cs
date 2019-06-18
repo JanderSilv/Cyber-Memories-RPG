804797dcfe6a9e5363cf2811ab2c6c5be3c1a65b
JanderSilv <jander.500@hotmail.com>
2019-06-18 02:38:02 -0300

    Sei la

--- RPG/RPG/Src/Scripts/Telas/MenuPrin.cs
+++ RPG/RPG/Src/Scripts/Telas/MenuPrin.cs
@@ -1,4 +1,5 @@
 ﻿using _3ReaisEngine.Core;
+using _3ReaisEngine.RPG.Components;
 using _3ReaisEngine.RPG.Core;
 using _3ReaisEngine.UI;
 using System;
@@ -12,46 +13,69 @@ using Windows.UI.Xaml.Media;
 public class MenuPrin : Window
 {
     public Window next;
-    public Window about;
+    public UPanel about;
     public MenuPrin(Page root) : base(root)
     {
                     /* Algoritmo do Menu */
 
+        Audio musicaDeFundo = new Audio();
+        AudioSource As = new AudioSource();
+        As.Name = "Src/Audio/Funkaholic.mp3";
+        As.Loop = false;
+        As.Volume = 50;
+
+        musicaDeFundo.Audios.Add("Musica", As);
+        musicaDeFundo.Play("Musica");
+
+
         UImage logo = new UImage("Src/Images/Menu/Logo2.png", new Vector2(50, 22), new Vector2(570, 185));
+        UImage background = new UImage("Src/Images/Menu/Botões/Mountains1.png", new Vector2(50, 50), new Vector2(1000,1000));
 
         UButton btn_Jogar = new UButton("", new Vector2(50, 48), new Vector2(229, 86), Play);
         UButton btn_Conf  = new UButton("", new Vector2(50, 67), new Vector2(229, 86));
         UButton btn_Sair  = new UButton("", new Vector2(50, 86), new Vector2(229, 86), Exit);
-        //UButton btn_Sobre = new UButton("", new Vector2(92, 92), new Vector2(103, 35), About);
+        UButton btn_Sobre = new UButton("", new Vector2(92, 92), new Vector2(103, 35), About);
 
         btn_Jogar.setBackground("Src/Images/Menu/Botões/Iniciar.png", Stretch.Uniform);
-        btn_Conf .setBackground("Src/Images/Menu/Botões/Configurações.png", Stretch.Uniform);
-        btn_Sair .setBackground("Src/Images/Menu/Botões/Sair.png", Stretch.Uniform);
+        btn_Conf.setBackground("Src/Images/Menu/Botões/Configurações.png", Stretch.Uniform);
+        btn_Sair.setBackground("Src/Images/Menu/Botões/Sair.png", Stretch.Uniform);
+        btn_Sobre.setBackground("Src/Images/Menu/Botões/Sobre.png");
 
         btn_Jogar.setOnHover("Src/Images/Menu/Botões/Jogar_Selecionado.png");
         btn_Conf.setOnHover("Src/Images/Menu/Botões/Configurações_Selecionado.png");
         btn_Sair.setOnHover("Src/Images/Menu/Botões/Sair_Selecionado.png");
+        btn_Sobre.setOnHover("Src/Images/Menu/Botões/Sobre_Selecionado.png");
 
+        Add(background);
         Add(logo);
-        //Add(btn_Sobre);
+        Add(btn_Sobre);
         Add(btn_Jogar);
         Add(btn_Conf);
         Add(btn_Sair);
 
         /* Algoritmo da aba Sobre */
 
-    //    UButton btn_Voltar = new UButton("", new Vector2(11, 7), new Vector2(145, 37), Voltar);
+        about = new UPanel(new Vector2(50, 50), new Vector2(800, 640));
 
-    //    btn_Voltar.setBackground("Src/Images/Menu/Botões/Voltar.png");
-    //    btn_Voltar.setOnHover("Src/Images/Menu/Botões/Voltar_Selecionado.png");
+        UButton btn_Voltar = new UButton("", new Vector2(11, 7), new Vector2(145, 37), Voltar);
+        
+        btn_Voltar.setBackground("Src/Images/Menu/Botões/Voltar.png");
+        btn_Voltar.setOnHover("Src/Images/Menu/Botões/Voltar_Selecionado.png");
 
-    //    about.Add(btn_Voltar);
+        about.SetBackGround("Src/Images/Menu/Botões/Sobre.jpg");
+        about.addChild(btn_Voltar);
     }
 
-    //private void Voltar(object sender)
-    //{
-    //    //
-    //}
+
+    private void About(object sender)
+    {
+        Add(about);
+    }
+
+    private void Voltar(object sender)
+    {
+        Remove(about);
+    }
 
     private void Exit(object sender)
     {
